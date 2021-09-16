a5d4c00c81584981d1f0acf113fb2c46fde52fb7
849961 <849961@SAIT227700IT.ACDM.DS.SAIT.CA>
2021-09-16 11:31:37 -0600

    Added one line of code. Added a print statement to print out string text

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String text = "Hello WOrld";
+        String text = "Hello World";
+        System.out.println(text);
     }
     
 }
